34de87a2fa87cb47695702e39a29e956bffbd53f
bfalad22 <bolasaliu93@gmail.com>
2023-10-07 04:07:18 -0700

    0x00-python_variable_annotations

--- 0-add.py
+++ 0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-"""
-Module that contains a single function that adds two integers.
+""" Description: type-annotated function add that takes float a and float b as arguments and returns their sum as a float
+Parameters: a: float
+            b: float
+
 """
 
 def add(a: float, b: float) -> float:
-    """ Add two integers together. """
+    "" Return sum of a and b. """
     return a + b
