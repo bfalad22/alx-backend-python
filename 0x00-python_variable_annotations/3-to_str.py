dd471644e21b20bb29b1616c2320c5b07148d5bb
bfalad22 <bolasaliu93@gmail.com>
2023-10-09 04:12:02 -0700

    0x00-python_variable_annotations

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Description: Accepts a float n as
+argument and returns the string
+representation of the float.
+Parameters: n: float
+"""
+
+
+
+
+
+def to_str(n: float) -> str:
+    """ Return n as a string. """
+    return str(n)
