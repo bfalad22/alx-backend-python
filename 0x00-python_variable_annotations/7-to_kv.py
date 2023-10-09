2a20de241a25b9b32bf0acb7b3a2d41a960854d1
bfalad22 <bolasaliu93@gmail.com>
2023-10-09 09:24:40 -0700

    0x00-python_variable_annotations

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Description: Accepts a string k
-and an int OR float v as arguments and 
+and an int OR float v as arguments and
 returns a tuple. The first element of
 the tuple is the string k.
 The second element is the square of the int/float
@@ -10,8 +10,6 @@ Parameters: k: str, v: Union[int, float]
 
 
 from typing import Union, Tuple
-
-
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
