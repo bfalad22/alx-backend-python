25d5fc5aceca47ae32862dc240c447d181ca9cef
bfalad22 <bolasaliu93@gmail.com>
2023-10-10 04:22:55 -0700

    1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
-""" Description: Import wait_random from the 
-previous python file that you've written and
-write an async routine called wait_n that takes
-in 2 int arguments: max_delay and n. You will
-spawn wait_random n times with the specified
-max_delay.
+''' Description: Import wait_random from the previous python file that
+                 you’ve written and write an async routine called wait_n
+                 that takes in 2 int arguments: max_delay and n. You will
+                 spawn wait_random n times with the specified max_delay.
 
-
-wait_n should return the list of all the
-delays(float values) The list of the delays
-should be in ascending order without using
-sort() because of concurrency.
-Arguments: n: int, max_delay: int = 10
-"""
-
+                 wait_n should return the list of all the delays(float values)
+                 The list of the delays should be in ascending order without
+                 using sort() because of concurrency.
+    Arguments: n: int, max_delay: int = 10
+'''
 
 import asyncio
 import random
@@ -21,7 +16,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max-delay: int = 10)  -> List[float]:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """ Waits for ran delay until max_delay, returns list of actual delays """
     spawn_ls = []
     delay_ls = []
@@ -29,7 +24,6 @@ async def wait_n(n: int, max-delay: int = 10)  -> List[float]:
         delayed_task = asyncio.create_task(wait_random(max_delay))
         delayed_task.add_done_callback(lambda x: delay_ls.append(x.result()))
         spawn_ls.append(delayed_task)
-
 
     for spawn in spawn_ls:
         await spawn
--- 0x01-python_async_function/1-main.py
+++ 0x01-python_async_function/1-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Test file for printing the corect output
+""" Test file for printing the correct output
 of the wait_n coroutine
 """
 
